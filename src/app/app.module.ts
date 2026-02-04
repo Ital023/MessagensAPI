44f61972974f2a868b50c50759af329de62ca0ef
Ital023 <contadoitalo911@gmail.com>
2026-02-04 18:46:11 -0300

    feat: typed configuration

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RecadosModule } from '../recados/recados.module';
+import { PessoasModule } from '../pessoas/pessoas.module';
+import { ConfigModule, ConfigService, ConfigType } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import appConfig from './app.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forFeature(appConfig)],
+      inject: [appConfig.KEY],
+      useFactory: async (appConfiguration: ConfigType<typeof appConfig>) => {
+        return {
+          type: appConfiguration.database.type,
+          host: appConfiguration.database.host,
+          port: appConfiguration.database.port,
+          username: appConfiguration.database.username,
+          database: appConfiguration.database.database,
+          password: appConfiguration.database.password,
+          autoLoadEntities: appConfiguration.database.autoLoadEntities,
+          synchronize: appConfiguration.database.synchronize,
+        };
+      },
+    }),
+    RecadosModule,
+    PessoasModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
